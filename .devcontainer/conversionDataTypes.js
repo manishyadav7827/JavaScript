cd7deb641c40243fb56ee027d84b6d72dfd37ada
manishyadav7827 <dd2manishyadav@gmail.com>
2024-07-28 04:10:39 +0000

    conversionDataTypes

---
+++ .devcontainer/conversionDataTypes.js
@@ -0,0 +1,19 @@
+let score =true
+console.log(typeof score)
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+//"25" =>25
+//"Manish" => Nan
+// true=>1
+// false=>0
+let isloggedIn=""
+let booleanIsloggedIn=Boolean(isloggedIn)
+//console.log(booleanIsloggedIn)
+ // 1=>true
+ //0=>false
+ let number=56
+ let stringnumber=String(number)
+ console.log(stringnumber)
+ console.log(typeof stringnumber)
