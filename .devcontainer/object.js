45aaa867d854f8aad37e7e527b35889d20e5e641
manishyadav7827 <dd2manishyadav@gmail.com>
2024-07-30 05:09:32 +0000

    object.js done

---
+++ .devcontainer/object.js
@@ -0,0 +1,15 @@
+const mySymb=Symbol("mykey1")
+const obj={
+    name:"Manish",
+    email: "manishyadav@microsoft.com",
+    [mySymb]:"mykey1"
+
+
+
+}
+console.log(obj)
+obj.email="manishyadav@Tcs.com"
+console.log(obj)
+Object.freeze(obj)
+obj.email="manish@cc.com"
+console.log(obj)
